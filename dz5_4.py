2d0f57b8a0ae31da5bdb9ac1769042bc5c246c13
Mykhailo2023 <152901832+Mykhailo2023@users.noreply.github.com>
2024-02-08 19:38:01 +0200

    0802

--- dz5_4.py
+++ dz5_4.py
@@ -1,102 +1,131 @@
-from colorama import Fore
 
-contacts = {}
+import os
+from colorama import Fore, Back
 
-# декоратор для обробки помилок введення користувачем
+
+# додаємо декоратор для обробки помилок
 def input_error(func):
     def inner(*args, **kwargs):
         function_name = str(func).split(" ")[1]
-        if len(args) != 2:  # Перевірка кількості аргументів
-            print(f"{Fore.RED}Неправильна кількість аргументів. Потрібно ввести ім'я та номер телефону.{Fore.RESET}")
-            return False
-        name, phone = args
-        if not phone.isdigit():  # Перевірка, чи номер є послідовністю цифр
-            print(f"{Fore.LIGHTRED_EX}Номер не є цифрами.{Fore.RESET}")
-            return False
         try:
             return func(*args, **kwargs)
+        # якщо помилка при введення команди add
         except ValueError:
             if function_name == "add_contact":
-                return "\nPlease, enter again your command to add contact correctly. \nGive me name and phone please\n"
-            elif function_name == "change_contact":
-                return "\nPlease, enter again your command to change contact correctly. \nGive me name and phone please\n"
+                return  f"{Fore.RED}Неправильно введена команда.\
+                    \nPlease type {Fore.LIGHTGREEN_EX} add name phone.{Fore.RESET}\n"
+            elif function_name == "change_phone":
+                return  f"{Fore.YELLOW}Неправильно введена команда.\
+                    \nПотрібно ввести {Fore.LIGHTGREEN_EX}change name new_phone.{Fore.RESET}\n"
+            
         except IndexError:
             if function_name == "show_phone":
-                return "\nPlease, enter again your command to show contact correctly. \nGive me name please\n"
+                return f"{Fore.YELLOW}Неправильно введена команда.\
+                    \nПотрібно ввести {Fore.LIGHTGREEN_EX}phone name{Fore.RESET}\n"
+                    
         except KeyError:
+            # виводить помилку, якщо немає такого контакту
             if function_name == "show_phone":
-                return "\nThere is no such contact yet. Add it please.\n"
-        except Exception as e:
-            print(f"An error occurred: {e}")
+                return f"Контакту {name_out(name)} не існує. Add it please.\n"
+            
     return inner
 
-
-
-# функція розбиття введення користувача на
-# окрема команду cmd та
-# аргументи *args, введені після команди
+# функція взаємодії з користувачем
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
-    return cmd, *args
+    return cmd, args
 
-# функція додавання контакту до бази
+# функція виводу іʼмя у відповідях боту
+def name_out (name:str):
+    return f"{Fore.MAGENTA}{name}{Fore.RESET}"
+
+
 @input_error
-def add_contact(name, phone, contacts: {}):
-    contacts[name] = phone
-    return f"Contact {name} added.\n"
-
-# функція зміни номера контакту
-@input_error
-def change_contact(args, contacts: {}):    
-    contacts[name] = phone
-    return "Contact updated.\n"
-
-# функція показу номера контакта за його ім'ям
-@input_error
-def show_phone(args, contacts: {}):
-    name = args[0]
-    return f"Phone number for user {name} is -> {contacts[name]}\n"
-
-# функція показу всіх контактів бази
-def show_all(contacts: {}):
-    all_contacts=""
-    if len(contacts)>0:
-        count_number=1
-        for name, number in contacts.items():
-            all_contacts+=str(count_number)+" - "+name+" "+str(number)+"\n"
-            count_number+=1
-        return all_contacts
+# функція додавання контактів
+def add_contact(args, contacts):
+    name, phone = args
+    if phone.isdigit():
+        # перевіряємо чи вже існує такий номер або імя
+        if phone in contacts.values() or name in contacts.keys():
+                        
+            return f"{Fore.RED}Цей номер або ім'я вже присутній у контактах.{Fore.RESET}"
+        else:
+            contacts[name] = phone
+            return f"{Fore.GREEN}Контакт{Fore.RESET} {name_out(name)} {Fore.GREEN}додано.{Fore.RESET}"
     else:
-        return "\nYour contacts database is empty!\nAdd contacts please.\n"
+        return f"{Fore.LIGHTRED_EX}Номер не є цифрами.{Fore.RESET}"
 
-# головна логіка
+        
+# функція виводу всіх контактів    
+
+@input_error
+def all_contacts(contacts):
+    if contacts == {}:
+        return f"{Fore.LIGHTRED_EX}Warning:{Fore.RESET}Contacts list is empty."
+    else:
+        return f"{contacts}"
+
+
+# функція зміни номеру у словнику
+@input_error
+def change_phone(args, contacts):
+       
+    name, new_phone = args
+    # перевіряємо чи новий телефон скдадається з цифр
+    if not new_phone.isdigit():
+        return  f"{Fore.RED}Новий номер телефону повинен містити лише цифри.{Fore.RESET}"
+    # перевіряємо чи існує такий контакт
+    if name not in contacts:
+        return f"{Fore.YELLOW}Попередження: Контакт{Fore.RESET} {name_out(name)} {Fore.YELLOW}не знайдено.{Fore.RESET}"
+    
+    contacts[name] = new_phone
+    return f"{Fore.LIGHTGREEN_EX}Номер телефону контакту{Fore.RESET} {name_out(name)} {Fore.LIGHTGREEN_EX}змінено на {Fore.RESET} {Fore.LIGHTBLUE_EX}{new_phone}{Fore.RESET}"
+    
+ 
+
+# функція виводу номеру телефону за імʼям контакту
+@input_error
+
+def show_phone(args, contacts):
+    name=args[0]
+    if name in contacts:
+        return f"{Fore.GREEN}Телефон контакту{Fore.RESET} {name_out(name)} {Fore.GREEN}є{Fore.RESET} {Fore.LIGHTBLUE_EX}{contacts[name]}{Fore.RESET}"
+    else:
+        return f"{Fore.RED}Contact{Fore.RESET} {name_out(name)} {Fore.RED}not found{Fore.RESET}"
+
+# головна функція
 def main():
-    print("\nWelcome to the assistant bot!\n")
-    while True:
-        user_input = input("Enter your command: ")
-        if user_input is not None:
-            if  user_input in ["close", "exit"]:
-                print("\nGood bye!\n")
-                break
-            elif user_input=="hello":
-                print("\nHow can I help you?\n")
-            elif len(user_input)==0:
-                print("\nYou entered empty command! Please, try again.\n")
-                continue
-            else:
-                entered_command, *args = parse_input(user_input)
-                if entered_command=="add":
-                    print(add_contact(args, contacts))
-                elif entered_command=="change":
-                    print(change_contact(args, contacts))
-                elif entered_command=="phone":
-                    print(show_phone(args, contacts))
-                elif entered_command=="all" \
-                    and len(args)==0:
-                    print(show_all(contacts))
-                else:
-                    print("Invalid command.\n")
+    contacts = {}
+    
+    while True:      
+
+        user_input = input(f"{Back.LIGHTBLACK_EX}Введіть команду: {Back.RESET}")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print(f"{Back.LIGHTGREEN_EX}Good bye!{Back.RESET}")
+            break
+
+        elif command in ["hello", "привіт"]:
+            print(f"{Fore.GREEN}Як я можу вам допомогти?{Fore.RESET}")
+
+        elif command in ["add", "додати"]:
+            print(add_contact(args, contacts))
+            
+        elif command == "phone":
+            print(show_phone(args, contacts))           
+            
+        elif command == "all":
+            print(f"{Fore.CYAN}{all_contacts(contacts)}{Fore.RESET}")
+        
+        elif command == "change":
+            print(change_phone(args, contacts))
+        
+        else:
+            print(f"{Fore.LIGHTRED_EX}Невірна команда.{Fore.RESET}")
 
 if __name__ == "__main__":
     main()
+
+
--- te.py
+++ te.py
@@ -1,98 +1,132 @@
-from colorama import Fore
+import os
+from colorama import Fore, Back
 
-contacts = {}
 
-# декоратор для обробки помилок введення користувачем
+# Додаємо декоратор для обробки помилок
 def input_error(func):
     def inner(*args, **kwargs):
         function_name = str(func).split(" ")[1]
-        if len(args) != 2:  # Перевірка кількості аргументів
-            print(f"{Fore.RED}Неправильна кількість аргументів. Потрібно ввести ім'я та номер телефону.{Fore.RESET}")
-            return False
-        name, phone = args
-        if not phone.isdigit():  # Перевірка, чи номер є послідовністю цифр
-            print(f"{Fore.LIGHTRED_EX}Номер не є цифрами.{Fore.RESET}")
-            return False
         try:
             return func(*args, **kwargs)
         except ValueError:
-            if function_name == "add_contact":
-                return "\nPlease, enter again your command to add contact correctly. \nGive me name and phone please\n"
-            elif function_name == "change_contact":
-                return "\nPlease, enter again your command to change contact correctly. \nGive me name and phone please\n"
+            if function_name == "add":
+                return False, "Please, enter again your command to add contact correctly. \nGive me name and phone please"
+            elif function_name == "change":
+                return False, "Please, enter again your command to change contact correctly. \nGive me name and phone please"
         except IndexError:
-            if function_name == "show_phone":
-                return "\nPlease, enter again your command to show contact correctly. \nGive me name please\n"
+            if function_name == "phone":
+                return False, "Please, enter again your command to show contact correctly. \nGive me name please"
         except KeyError:
-            if function_name == "show_phone":
-                return "\nThere is no such contact yet. Add it please.\n"
-        except Exception as e:
-            print(f"An error occurred: {e}")
+            if function_name == "phone":
+                return False, "There is no such contact yet. Add it please."
     return inner
 
 
+def validate_phone(func):
+    def wrapper(args, contacts):
+        name, phone = args
+        if phone.isdigit():
+            return func(args, contacts)
+        else:
+            return False, "Номер не є цифрами."
+    return wrapper
+
+# Функція взаємодії з користувачем
 def parse_input(user_input):
-    parts = user_input.split()
-    cmd = parts[0].lower()
-    args = parts[1:]
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
     return cmd, args
 
-# функція додавання контакту до бази
-@input_error
-def add_contact(name, phone, contacts: {}):
-    contacts[name] = phone
-    return f"Contact added.\n"
+# Функція виводу імені у відповідях боту
+def name_out(name:str):
+    return f"{Fore.MAGENTA}{name}{Fore.RESET}"
 
-# функція зміни номера контакту
+# Декоратор для функції add_contact
 @input_error
-def change_contact(name, phone, contacts: {}):
-    contacts[name] = phone
-    return "Contact updated.\n"
+@validate_phone
 
-# функція показу номера контакта за його ім'ям
-@input_error
-def show_phone(name, contacts: {}):
-    return f"Phone number for user {name} is -> {contacts[name]}\n"
-
-# функція показу всіх контактів бази
-def show_all(contacts: {}):
-    all_contacts=""
-    if len(contacts)>0:
-        count_number=1
-        for name, number in contacts.items():
-            all_contacts+=str(count_number)+" - "+name+" "+str(number)+"\n"
-            count_number+=1
-        return all_contacts
+def add_contact(args, contacts):
+    name, phone = args
+    if phone in contacts.values():
+        return False, "Цей номер вже присутній у контактах."
     else:
-        return "\nYour contacts database is empty!\nAdd contacts please.\n"
+        contacts[name] = phone
+        return True, f"Контакт {name_out(name)} додано."
+    
 
-# головна логіка
+
+# Декоратор для функції change_phone
+@input_error
+@validate_phone
+def change_phone(args, contacts):
+    if len(args) != 2:
+        return False, "Неправильна кількість аргументів. Потрібно ввести ім'я та новий номер телефону."
+    
+    name, new_phone = args
+    
+    if name not in contacts:
+        return False, f"Попередження: Контакт {name_out(name)} не знайдено."
+
+# Функція виводу всіх контактів    
+@input_error
+def all_contacts(contacts):
+    if contacts == {}:
+        return False, "Увага: Контактні списки порожні."
+    else:
+        return True, f"{contacts}"
+
+# Функція виводу номеру телефону за іменем контакту
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return True, contacts[name]
+    return False, f"Contact {name_out(name)} not found"
+
 def main():
-    print("\nWelcome to the assistant bot!\n")
-    while True:
-        user_input = input("Enter your command: ")
-        if user_input is not None:
-            if  user_input in ["close", "exit"]:
-                print("\nGood bye!\n")
-                break
-            elif user_input=="hello":
-                print("\nHow can I help you?\n")
-            elif len(user_input)==0:
-                print("\nYou entered empty command! Please, try again.\n")
-                continue
+    contacts = {}
+    
+    while True:      
+        user_input = input(f"{Back.LIGHTBLACK_EX}Введіть команду: {Back.RESET}")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print(f"{Back.LIGHTGREEN_EX}Good bye!{Back.RESET}")
+            break
+
+        elif command in ["hello", "привіт"]:
+            print(f"{Fore.GREEN}Як я можу вам допомогти?{Fore.RESET}")
+
+        elif command == "add":
+            success, message = add_contact(args, contacts)
+            if success:
+                print(f"{Fore.CYAN}Успішно: {message}{Fore.RESET}")
             else:
-                entered_command, *args = parse_input(user_input)
-                if entered_command=="add":
-                    print(add_contact(*args, contacts))
-                elif entered_command=="change":
-                    print(change_contact(*args, contacts))
-                elif entered_command=="phone":
-                    print(show_phone(*args, contacts))
-                elif entered_command=="all" \
-                    and len(args)==0:
-                    print(show_all(contacts))
-                else:
-                    print("Invalid command.\n")
+                print(f"{Fore.RED}Помилка: {message}{Fore.RESET}")
+
+        elif command == "change":
+            success, message = change_phone(args, contacts)
+            if success:
+                print(f"{Fore.CYAN}Успішно: {message}{Fore.RESET}")
+            else:
+                print(f"{Fore.RED}Помилка: {message}{Fore.RESET}")
+
+        elif command == "all":
+            success, message = all_contacts(contacts)
+            if success:
+                print(f"{Fore.CYAN}Успішно: {message}{Fore.RESET}")
+            else:
+                print(f"{Fore.RED}Помилка: {message}{Fore.RESET}")
+
+        elif command == "phone":
+            success, message = show_phone(args, contacts)
+            if success:
+                print(f"{Fore.CYAN}Успішно: {message}{Fore.RESET}")
+            else:
+                print(f"{Fore.RED}Помилка: {message}{Fore.RESET}")
+
+        else:
+            print(f"{Fore.YELLOW}Невідома команда. Будь ласка, спробуйте ще раз.{Fore.RESET}")
 
 if __name__ == "__main__":
     main()
