c9ee2cd8a02984050bcf5ec81738801262d8c7f8
Mykhailo2023 <152901832+Mykhailo2023@users.noreply.github.com>
2024-02-13 12:05:04 +0200

    1302_1

--- dz5_2.py
+++ dz5_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 from typing import Callable
@@ -17,28 +16,6 @@ def sum_profit(text: str, func: Callable):
     total_sum = sum(numbers)
     return total_sum
 
-text = "Загальний дохід працівника складається з декількох  частин: 1000.01 як основний o19 дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = "Загальний дохід працівника  складається or 25 з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
-=======
-import re
-from typing import Callable
-
-def generator_numbers(text: str):
-    # визначення дійсних чисел
-    pattern = re.compile(r'\b\d+(\.\d+)?\b')
-    matches = pattern.finditer(text)
-    # проходио по знайдених числах та повертаємо генератор
-    for match in matches:
-        yield float(match.group())
-
-def sum_profit(text: str, func: Callable):
-    # підсумовуємо числа
-    total_sum = sum(func(text))
-    return total_sum
-
-
-text = "Загальний дохід працівника  складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
->>>>>>> ae52bfbc40e7c9b6ccb9fd6b5bc5a9e055068313
